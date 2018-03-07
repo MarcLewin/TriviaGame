3019e6a25cfc212c97db8fbca47b15ba776dda79
Your Name <you@example.com>
2018-03-06 20:55:05 -0500

    index done, js started

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
 var questions = [{
+    question: "What is Peter Pan's Motto?",
+	answerList: ["I love pirates", "All children but one grow up", "Please feed me to a crocodile", "I love Captain Hook"],
+	answer: 1
+},{
+	question: "Who plays Peter Pan in Hook?",
+	answerList: ["Robin Williams", "Dustin Hoffman", "Chrstian Bale", "Christopher Walken"],
+	answer: 0
+},{
+	question: "Where is Peter Pan from?",
+	answerList: ["Chicago", "The Bronx", "Neverland", "The Lost world"],
+	answer: 2
+},{
 
 }];
